ffb6b3044becae9203030f4d581566ce06bcf362
Christian Meyer <christian.meyer@bbv.ch>
2013-06-07 21:21:42 +0200

    Add license information
    - Add license headers to existing code
    - Add about dialog

--- hebi_no_shisho/shisho.py
+++ hebi_no_shisho/shisho.py
@@ -1,3 +1,21 @@
+"""
+    Hebi no Shisho - A small scale pythonic library management tool
+    Copyright (C) 2013 Christian Meyer
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 import database
 import pygtk
 pygtk.require('2.0')
@@ -20,6 +38,6 @@ class MainWindow():
         dic = { "on_FileMenuQuit_activate" : gtk.main_quit}
         self.builder.connect_signals(dic, None)
         
-        window = self.builder.get_object("MainWindow")
+        window = self.builder.get_object("winMain")
         window.show()
         return
